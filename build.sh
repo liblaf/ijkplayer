900025d597787a180bf42410f8783418cdc3f22c
Qin Li <30631553+liblaf@users.noreply.github.com>
2022-11-22 22:29:07 +0800

    build: update

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 set -o errexit
 set -o nounset
+set -o pipefail
 
 export ANDROID_SDK="${HOME}/Android/Sdk/build-tools/25.0.3"
 export ANDROID_NDK="${HOME}/Android/Sdk/ndk/22.0.7026061"
 
 cd config
 rm --force module.sh
-ln --symbolic module-default.sh module.sh
+ln --symbolic module-lite.sh module.sh
 cd ..
 
 cd android/contrib
